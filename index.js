6ad645ae39c5ea22bc872ee8650b9228c7583c8c
Kenneth <14309096+krusli@users.noreply.github.com>
2018-03-24 10:54:02 +1100

    Read env vars

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', function(req, res) {
     res.send('Hello world!');
 });
 
-app.listen(3000, function() {   // TODO change this to port 80 before deployment
-    console.log('Listening at port 3000.');
-});
+if (process.env.ENV = 'PRODUCTION') {
+    app.listen(80, () => console.log('Listening at port 80.');
+}
+else {
+    app.listen(3000, function() {   // same as () => {} (arrow function)
+        console.log('Listening at port 3000.');
+    });
+}
