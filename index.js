e38d5f2b11bc3988c4800b042ff9e96503124183
Kenneth <14309096+krusli@users.noreply.github.com>
2018-03-24 14:57:24 +1100

    More cleanup

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
+/* imports */
+const path = require('path');
 const express = require('express');   // imports express
 const app = express();
-
-const path = require('path');
-// const mongoose = require('mongoose');
-// const winston = require('winston');
 
 const globals = require('./globals');
 const mongoose = globals.mongoose;
 const winston = globals.winston;
 
 /* routes */
-app.get('/', function(req, res) {
-  // res.send('Hello world!');
-  res.sendFile(path.join(__dirname, 'views/index.html'));
-});
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'views/index.html')));
 
 app.use('/users', require('./controllers/users'));
 
@@ -21,16 +17,16 @@ app.use('/users', require('./controllers/users'));
 app.use(express.static(path.join(__dirname, 'static')));
 
 // 404 handler
-app.use((req, res, next) => res.status(404).send("Error 404: not found"));
+app.use((req, res, next) => res.status(404).send("Error 404: page not found"));
 
 // error handler
-app.use(function errorHandler (err, req, res, next) {
+app.use((err, req, res, next) => {
   if (res.headersSent) {
-    return next(err)
+    return next(err);
   }
-  res.status(500)
-  res.render('error', { error: err })
-})
+  res.status(500);
+  res.render('error', { error: err });
+});
 
 /* start the server */
 let port = process.env.PORT || 3000;
