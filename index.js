3374c06e78d42bd91002e63479eaf9767185bb60
Kenneth <14309096+krusli@users.noreply.github.com>
2018-03-24 13:47:54 +1100

    Now using Bootstrap

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-var express = require('express');   // imports express
-var app = express();
+const express = require('express');   // imports express
+const app = express();
+const path = require('path');
 
 app.get('/', function(req, res) {
-    res.send('Hello world!');
+  // res.send('Hello world!');
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-if (process.env.ENV = 'PRODUCTION') {
-    let port = process.env.PORT || 3000;
-    app.listen(port, () => console.log('Listening at port ' + port + ' on Heroku.'));
-}
-else {
-    app.listen(3000, function() {   // same as () => {} (arrow function)
-        console.log('Listening at port 3000.');
-    });
-}
+// static files
+app.use(express.static(path.join(__dirname, 'static')));
+
+// 404 handler
+app.use((req, res, next) => res.status(404).send("Error 404: not found"));
+
+// error handler
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+})
+
+/* start the server */
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Listening at port ' + port));
